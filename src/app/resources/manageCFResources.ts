9d1f64aff0956735e3093b467fafc35d5500e57b
Abinand P <abinand0911@gmail.com>
2025-03-05 13:38:37 +0530

    exported the default functions
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- src/app/resources/manageCFResources.ts
+++ src/app/resources/manageCFResources.ts
@@ -1,12 +1,10 @@
-
-
-export const intrinsicFunctionType = (value:any) =>{
-    if(!value || typeof value !== 'object' || Object.keys(value).length !== 1){
-        return null;
-    }
-    const [key] = Object.keys(value);
-    if(key === 'Ref' || key.startsWith('Fn::')){
-        return key;
-    }
+export const intrinsicFunctionType = (value: any) => {
+  if (!value || typeof value !== 'object' || Object.keys(value).length !== 1) {
     return null;
-}
+  }
+  const [key] = Object.keys(value);
+  if (key === 'Ref' || key.startsWith('Fn::')) {
+    return key;
+  }
+  return null;
+};
